eb7f058dc2fdeea9a82a38dc9d8ec5adf1aa7ab5
alex_mihai <sdva@gmail.com>
2024-12-20 15:38:59 +0200

    sp

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ async def get_openweathermap_data(url, city):
             status_code = response.status
 
             if status_code == 404:
+                # TODO find a way to figure out for sure if the 404 is caused by a wrong city name.
+                #  404 can be caused by a change in the root URL made by the weather data provider
                 logger.debug(f'A request was made for a city that does not exist {city}')
                 raise CityDoesNotExist(city)
 
@@ -33,6 +35,11 @@ async def get_weather_data(city):
 
 @app.get("/weather")
 async def weather(city):
+    """
+    Returns weather data for a specific city name.
+    :param city: str
+    :return:
+    """
 
     try:
         result = await get_weather_data(city)
@@ -48,7 +55,7 @@ async def weather(city):
             'error': error.message
         }
         status_code = 404
-        
+
     finally:
         pass
 
