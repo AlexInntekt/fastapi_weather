5a2449f36484246b497ab020c7964e2eb07b8812
alex_mihai <sdva@gmail.com>
2024-12-22 13:09:23 +0200

    improved comments

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import settings
 from utils.logging import get_logger
 from routes.routes import router
 
-
 logger = get_logger(__name__)
+
 
 
 app = FastAPI()
@@ -21,7 +21,3 @@ app.include_router(router)
 
 if __name__=='__main__':
     logger.debug(f'Running main with settings: {settings.module_name}')
-
-    # in case you dont want to use 'fastapi dev main.py', pipenv install uvicorn and then:
-    # import uvicorn
-    # uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- services/weather_service.py
+++ services/weather_service.py
@@ -22,16 +22,20 @@ class WeatherService():
             found_cache, file_path = await cache_manager.get_cache(city=city)
 
             if found_cache:
+                # this implies that there is a cache and is not older than n minutes (set n in settings)
                 logger.info(f"Retrieved cached data for city: {city} from S3. Cache Path: {file_path}")
                 weather_data = found_cache
             else:
+                # make API request and cache it on S3
                 logger.info(f"Caching weather data for city: {city} into S3")
-                weather_data_manager = WeatherSourceManager.factory(self.data_source, city)
-                weather_data = await weather_data_manager.get_weather_data()
+                weather_data_manager = WeatherSourceManager.factory(self.data_source, city) # request weather data externally
+                weather_data = await weather_data_manager.get_weather_data() # cache the results
 
                 file_path = await cache_manager.cache_to_s3(city, weather_data)
 
         else:
+            # enter here when S3 CDN is not used as cache,
+            # so each user request will imply an API request to the weather data provider
             weather_data_manager = WeatherSourceManager.factory(self.data_source, city)
             weather_data = await weather_data_manager.get_weather_data()
 
